bab7048d6a30c7b472c51df24a43a0cd085b4647
AmitxParmar <amitparmar901@gmail.com>
2023-08-08 22:45:47 +0530

    feat: backe-end: addImageMessage

--- server/controllers/MessageController.js
+++ server/controllers/MessageController.js
@@ -75,3 +75,31 @@ export const getMessage = async (req, res, next) => {
     next(err);
   }
 };
+
+export const addImageMessage = async (req, res, next) => {
+  try {
+    if (req.file) {
+      const date = new Date.now();
+      let fileName = "uploads/images" + date + req.file.originalName;
+      renameSync(req.file.path, fileName);
+      const prisma = getPrismaInstance;
+      const { from, to } = req.query;
+
+      if (from && to) {
+        const message = await prisma.messages.create({
+          data: {
+            message: fileName,
+            sender: { connect: { id: parseInt(from) } },
+            reciever: { connect: { id: parseInt(to) } },
+            type: "image",
+          },
+        });
+        return res.status(201).json({ message });
+      }
+      return res.status(400).send("From, to is required");
+    }
+    return res.status(400).send("Image is required");
+  } catch (err) {
+    next(err);
+  }
+};
